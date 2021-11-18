e59b90b6bc1566ed57bc42be1698ce25bbdbd066
Xiaokui Shu <subbyte@gmail.com>
2021-11-17 21:30:05 -0500

    init code

---
+++ cbm.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+CLIPBOARD_QUEUE_FILE = "~/.clipboard"
+
+import os
+import argparse
+import pyperclip
+
+def update_clipboard(backward: bool):
+    try:
+        with open(os.path.expanduser(CLIPBOARD_QUEUE_FILE)) as cbf:
+            messages = cbf.read().splitlines()
+            if backward:
+                messages.reverse()
+            it = iter(messages)
+    except:
+        it = iter([])
+
+    current_message = pyperclip.paste()
+
+    while True:
+        try:
+            message = next(it)
+            if message == current_message:
+                new_message = next(it)
+                break
+        except StopIteration:
+            new_message = None
+            break
+
+    if new_message:
+        pyperclip.copy(new_message)
+
+if __name__ == "__main__":
+
+    note = f"A smart clipboard manager that iterate over {CLIPBOARD_QUEUE_FILE} if the current clipboard is exactly one line of it."
+    parser = argparse.ArgumentParser(description=note)
+    parser.add_argument(
+        "-b", "--backward", help="get an item prior to the current one", action="store_true"
+    )
+    args = parser.parse_args()
+
+    update_clipboard(args.backward)
